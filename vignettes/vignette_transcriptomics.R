d963ba8666e2278e989d0050263603b3ecc20489
NicWir <mail.nicowirth@gmail.com>
2025-05-06 21:01:15 +0200

    built vignettes

--- vignettes/vignette_transcriptomics.R
+++
@@ -1,9 +0,0 @@
-## ----library, echo = T, eval = F, results = "hide"----------------------------
-# # Load the VisomX package
-# library(VisomX)
-
-## ----libraryCode, echo = F, eval = T, include = F, results = "hide"-----------
-# Load the VisomX package
-suppressPackageStartupMessages(library(VisomX))
-suppressPackageStartupMessages(library(ggplot2))
-
